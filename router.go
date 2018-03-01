28f584767dca2159df9f847007f18a7294a03084
David Bond <david@david-bond.net>
2018-03-01 19:53:08 +0000

    merged router.go & routes.go

--- router.go
+++ router.go
@@ -25,6 +25,13 @@ type (
 		log      *logrus.Logger
 	}
 
+	// The Route type defines a route that can be used by the router.
+	Route struct {
+		handler HandlerFunc
+		method  string
+		headers map[string]string
+	}
+
 	// The HandlerFunc type defines what a handler function should look like.
 	HandlerFunc func(events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
 
@@ -141,4 +148,49 @@ func (r *Router) recover(req events.APIGatewayProxyRequest) {
 			r.recovery(req, err)
 		}
 	}
+}
+
+// Headers allows you to specify headers a request should have in order to
+// use this route.
+func (r *Route) Headers(pairs ...string) *Route {
+	// Loop through the headers
+	for i := 0; i < len(pairs); i += 2 {
+		// If we have an odd number of pairs, skip the last one.
+		if len(pairs) < i+1 {
+			break
+		}
+
+		key := pairs[i]
+		value := pairs[i+1]
+
+		// Register the required header
+		r.headers[key] = value
+	}
+
+	return r
+}
+
+// CanRoute determines if the incoming request should use this route.
+func (r *Route) canRoute(req events.APIGatewayProxyRequest) bool {
+	// Loop through the expected headers & values
+	for expKey, expValue := range r.headers {
+		// If the header key is no present, we don't support this request
+		if _, ok := req.Headers[expKey]; !ok {
+			return false
+		}
+
+		// If the value is not what we expect from this key, we don't support
+		// this request.
+		if value := req.Headers[expKey]; value != expValue {
+			return false
+		}
+	}
+
+	// If the request method does not match this route's method, we don't
+	// support this request.
+	if req.HTTPMethod != r.method {
+		return false
+	}
+
+	return true
 }
